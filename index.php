103aee7718b8cde46c205bb53ac0f4f195f364e0
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2013-03-26 16:19:42 +0100

    add DOCTYPE
    (to ensure IE does not enter quirks mode)

--- index.php
+++ index.php
@@ -200,6 +200,7 @@ function handleAction($ether, $etherAuthorID, $action) {
 
 function html_start() {
   echo <<<EOF
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
 <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
